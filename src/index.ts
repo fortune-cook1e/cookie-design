22e546e0a64d8bc9e2778e5990f566bfb6dd0a72
fortune cookie <767077147@qq.com>
2022-10-04 19:33:56 +0800

    chore: 入口增加导出

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ import './styles/index.less'
 // 遮罩层
 export { default as Overlay } from '@/packages/Overlay'
 export type { OverlayProps } from '@/packages/Overlay'
+
+// Popup
+export { default as Popup } from '@/packages/Popup'
+export type { PopupProps } from '@/packages/Popup/types'
